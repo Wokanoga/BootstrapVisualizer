5d6e61892d944a4389677c6da7d7dd54e5dbbee5
Lincoln MacKay <lincoln@Lincoln-MBP.local>
2019-11-01 10:28:36 -0700

    CSS innerHTML now changes

--- script/javaScript.js
+++ script/javaScript.js
@@ -3,6 +3,7 @@ const button = document.getElementById('cssButton');
 button.addEventListener('click', function () {
     if (document.getElementById('style')) {
         document.getElementById('style').remove();
+        button.innerHTML='CSS IS OFF';
     } else {
         const head = document.getElementsByTagName('HEAD')[0];
         const link = document.createElement('link');
@@ -10,5 +11,6 @@ button.addEventListener('click', function () {
         link.rel = 'stylesheet';
         link.href = 'style/style.css';
         head.appendChild(link);
+        button.innerHTML='CSS IS ON';
     }
 });
